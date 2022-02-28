7ee79543a59cf651c05373c71eef9961bd39b703
Mikhail Knyazhev <markus621@gmail.com>
2022-03-01 02:11:41 +0300

    update deb config

--- Makefile
+++ Makefile
@@ -1,16 +1,12 @@
 SHELL=/bin/bash
 
-.PHONY: run_back run_front
+.PHONY: run_back
 run_back:
 	go run -race cmd/uri-one/main.go run -config=./configs/config.dev.yaml
-run_front:
-	cd web && npm ci --no-delete --cache=/tmp && npm run start
 
 .PHONY: build_back build_font
 build_back:
-	bash scripts/build.sh back
-build_font:
-	bash scripts/build.sh front
+	bash scripts/build.sh amd64
 
 .PHONY: linter
 linter:
@@ -30,7 +26,6 @@ develop_down:
 ci:
 	bash scripts/ci.sh
 
-.PHONY: ci
-#deb: build_font build_back
+.PHONY: deb
 deb: build_back
 	deb-builder build
--- scripts/build.sh
+++ scripts/build.sh
@@ -5,28 +5,9 @@ GOMAIN="$PWD/cmd/uri-one"
 
 cd $PWD
 
-back() {
-  rm -rf $PRJROOT/build/bin/uri-one_*
+rm -rf $PRJROOT/build/bin/uri-one_$1
 
-  go generate ./...
+go generate ./...
 
-  GO111MODULE=on GOOS=linux GOARCH=amd64 go build -o $PRJROOT/build/bin/uri-one_amd64 $GOMAIN
-  GO111MODULE=on GOOS=windows GOARCH=amd64 go build -o $PRJROOT/build/bin/uri-one_windows64.exe $GOMAIN
-  GO111MODULE=on GOOS=darwin GOARCH=amd64 go build -o $PRJROOT/build/bin/uri-one_macos $GOMAIN
-}
+GO111MODULE=on GOOS=linux GOARCH=$1 go build -o $PRJROOT/build/bin/uri-one_$1 $GOMAIN
 
-front() {
-  cd web && npm ci --no-delete --cache=/tmp && npm run build
-}
-
-case $1 in
-back)
-  back
-  ;;
-front)
-  front
-  ;;
-*)
-  echo "front or back"
-  ;;
-esac
